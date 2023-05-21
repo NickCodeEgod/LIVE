660d671471547778f8df49392f1d918bdc090f64
NickCodeEgod <123401332+NickCodeEgod@users.noreply.github.com>
2023-05-21 21:19:04 +0400

    LIVE

---
+++ assets/js/app.js
@@ -0,0 +1,117 @@
+const dataArray = [
+    {
+        userId: 1,
+        userName: 'სახელი 1',
+        userLastName: 'გვარი 1',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 20,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 2,
+        userName: 'სახელი 2',
+        userLastName: 'გვარი 2',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 30,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 3,
+        userName: 'სახელი 3',
+        userLastName: 'გვარი 3',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 35,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 4,
+        userName: 'სახელი 4',
+        userLastName: 'გვარი 4',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 29,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 5,
+        userName: 'სახელი 5',
+        userLastName: 'გვარი 5',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 49,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 6,
+        userName: 'სახელი 6',
+        userLastName: 'გვარი 6',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 31,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+    {
+        userId: 7,
+        userName: 'სახელი 7',
+        userLastName: 'გვარი 7',
+        userImage: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+        userAge: 31,
+        userAddresses: [
+            'address 1',
+            'address 2',
+            'address 3'
+        ]
+    },
+   
+];
+
+const container = document.getElementById('user-container');
+
+dataArray.forEach(data => {
+    const userCard = document.createElement('div');
+    userCard.classList.add('user-card');
+
+    const userImage = document.createElement('img');
+    userImage.src = data.userImage;
+    userImage.classList.add('user-image');
+    userCard.appendChild(userImage);
+
+    const userName = document.createElement('h3');
+    userName.textContent = data.userName + ' ' + data.userLastName;
+    userCard.appendChild(userName);
+
+    const userAge = document.createElement('p');
+    userAge.textContent = 'Age: ' + data.userAge;
+    userCard.appendChild(userAge);
+
+    const userAddresses = document.createElement('ul');
+    data.userAddresses.forEach(address => {
+        const listItem = document.createElement('li');
+        listItem.textContent = address;
+        userAddresses.appendChild(listItem);
+    });
+    userCard.appendChild(userAddresses);
+
+    container.appendChild(userCard);
+});
